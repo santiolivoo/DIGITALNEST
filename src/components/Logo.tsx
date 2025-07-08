d49e1e77c52e69e0ca537ff16e885894839f8ac3
santiolivoo <santiagoolivomarin@gmail.com>
2025-07-08 15:51:27 -0300

    commit 8/7 15:51

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="/20250707_1303_Logo de DigitalNest_simple_compose_01jzjsyeqefqgt6pys3gew7nn2.png"
+      src="\digitalnest.svg.svg"
       alt="DigitalNest Logo"
       width={480}
       height={160}
