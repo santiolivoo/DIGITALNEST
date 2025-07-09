c1e6aecb8832694d5a69caf297e8599808a6adb1
santiolivoo <santiagoolivomarin@gmail.com>
2025-07-09 15:36:18 -0300

    commit 9/7 15:36

---
+++ prisma/migrations/20250709183531_add_plantilla/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `plantilla` on the `Tienda` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Tienda" DROP COLUMN "plantilla";
